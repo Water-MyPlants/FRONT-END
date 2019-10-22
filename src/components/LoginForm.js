5b8f550317d51ade325a8894c9c10a4078b75c0e
Ayomide Jones <ayormeday@gmail.com>
2019-10-22 23:55:42 +0100

    revert form from formik

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,62 +1,65 @@
 import React from "react";
-import { Form, Field, withFormik } from "formik";
-import * as Yup from "yup";
+import { connect } from "react-redux";
+import * as actionCreators from "../state/actionCreators";
+import * as withAuth from "../helpers/axiosWithAuth";
 import { Link } from "react-router-dom";
-import * as actionCreators from '../state/actionCreators';
-// import * as withAuth from '../helpers/withAuth';
 
-const LoginForm = ({ errors, touched, values, status, history, login, onLoginInputChange, attemptLogin, logout }) => {
-  
-  
-  
-  return (
-    // Login Form with validation using Yup for Formik //
-
+export const LoginForm = ({
+  history,
+  login,
+  onLoginInputChange,
+  attemptLogin,
+  logout
+}) => {
+  const onLogin = e => {
+    e.preventDefault();
+    if (login.username.length === 0 || login.password.length === 0)
+      return false;
+    attemptLogin(login);
+    history.push("/plants");
+  };
+  const onInputChange = e => {
+    onLoginInputChange(e.target);
+  };
+  const onLogout = () => {
+    logout();
+    history.push("/");
+  };
+  return withAuth.isLoggedIn() ? (
+    <button onClick={onLogout}>Logout</button>
+  ) : (
     <div id="login-form" className="form-container">
-      <Form className="form">
-        <label>User Name:</label>
-        <Field type="text" name="userName" placeholder="User Name" />
-        <small>(Between 4-16 characters)</small>
-        {touched.userName && errors.userName && (
-          <span className="error">{errors.userName}</span>
-        )}
-        <label>Password:</label>
-        <Field type="password" name="password" placeholder="Password" />
-        <small>(Between 4-16 characters)</small>
-        {touched.password && errors.password && (
-          <span className="error">{errors.password}</span>
-        )}
+      <form className="form" onSubmit={onLogin}>
+        <label name="username">Username</label>
+        <input
+          id="username"
+          name="username"
+          type="text"
+          placeholder="Username"
+          value={login.username}
+          onChange={onInputChange}
+        />
+        <label name="password">Password </label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          placeholder="Password"
+          value={login.password}
+          onChange={onInputChange}
+        />
         <button className="btn" type="submit">
           LOG IN
         </button>
-      </Form>
+      </form>
       <Link className="form-link" to="/signup">
         Dont have an account?
       </Link>
     </div>
-    //!!! Login Form with validation using Yup for Formik //
   );
 };
 
-// withFormik validation and Yup Error Messages //
-const FormikLoginForm = withFormik({
-  mapPropsToValues({ userName, password }) {
-    return {
-      userName: userName || "",
-      password: password || ""
-    };
-  },
-
-  validationSchema: Yup.object().shape({
-    userName: Yup.string()
-      .min(4, "Need atleast 4 characters")
-      .max(16, "No more than 16 characters")
-      .required("User Name is required"),
-    password: Yup.string()
-      .min(4, "Need atleast 4 characters")
-      .max(16, "No more than 16 characters")
-      .required("Password is required")
-  })
-})(LoginForm);
-//!!! withFormik validation and Yup Error Messages //
-export default FormikLoginForm;
+export default connect(
+  state => state,
+  actionCreators
+)(LoginForm);
